9437031eaa208a978621aac35983c0a4acfd8cef
Amy Rivera <107879546+AR1190@users.noreply.github.com>
2022-06-22 19:46:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+computerNumber = random.randint(0,10)
+userGuess = int(input("Guess what number I am thinking of between 0 - 10: "))
+if (userGuess == computerNumber):
+  print("You guessed it right") 
+else:
+  print("Wrong! My number was " + str(computerNumber) + ".")
